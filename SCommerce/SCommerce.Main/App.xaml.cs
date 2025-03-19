76a5f09b9791562f2ea77f12cc81b6cf22cc212e
rbd <rbd@cin.ufpe.br>
2025-03-19 16:01:45 -0300

    02- App UI 02- Page

--- SCommerce/SCommerce.Main/App.xaml.cs
+++ SCommerce/SCommerce.Main/App.xaml.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+ * Copyright 2024 by Samsung Eletrônica da Amazônia Ltda. Todos os direitos reservados.
+ *
+ * Este software e seus códigos são confidenciais ("Informações Confidenciais")
+ * e de propriedade da Samsung Eletrônica da Amazônia Ltda.
+ * Você não deverá divulgar as Informações Confidenciais e deverá utilizá-las apenas de acordo
+ * com os termos do acordo de licença entre você e a Samsung Eletrônica da Amazônia Ltda.
+ *
+ * Copyright 2024 by Samsung Eletrônica da Amazônia Ltda. All rights reserved.
+ *
+ * This software and its code are confidential ("Confidential Information")
+ * and proprietary information of Samsung Eletrônica da Amazônia Ltda.
+ * You shall not disclose such Confidential Information and shall use it only in accordance
+ * with the terms of the license agreement you entered into with Samsung Eletrônica da Amazônia Ltda.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -15,6 +31,7 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Shapes;
+using SCommerce.Main.Views;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,6 +43,14 @@ namespace SCommerce.Main
     /// </summary>
     public partial class App : Application
     {
+        #region Private Fields
+
+        private Window? m_window;
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -35,6 +60,16 @@ namespace SCommerce.Main
             this.InitializeComponent();
         }
 
+        #endregion Public Constructors
+
+        #region Protected Properties
+
+        protected Frame RootFrame { get; private set; }
+
+        #endregion Protected Properties
+
+        #region Protected Methods
+
         /// <summary>
         /// Invoked when the application is launched.
         /// </summary>
@@ -42,9 +77,12 @@ namespace SCommerce.Main
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
         {
             m_window = new MainWindow();
+            RootFrame = new Frame();
+            RootFrame.Navigate(typeof(ProductDetailsPage));
+            m_window.Content = RootFrame;
             m_window.Activate();
         }
 
-        private Window? m_window;
+        #endregion Protected Methods
     }
 }
